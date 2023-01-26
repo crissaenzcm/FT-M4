d5cfdeb690a6e3cfc27f0dcd9023011abffa80b2
Cristian Saenz <cristiansaenz@Cristians-MacBook-Pro.local>
2023-01-26 14:55:50 -0500

    Homework Sequelize part I DONE

--- 03-sequelize/homework/db/index.js
+++ 03-sequelize/homework/db/index.js
@@ -1,11 +1,17 @@
-const { Sequelize, Op } = require('sequelize');
-const modelCharacter = require('./models/Character.js');
-const modelAbility = require('./models/Ability.js');
-const modelRole = require('./models/Role.js');
+const { Sequelize, Op } = require("sequelize");
+const modelCharacter = require("./models/Character.js");
+const modelAbility = require("./models/Ability.js");
+const modelRole = require("./models/Role.js");
+require("dotenv").config();
 
-const db = new Sequelize('postgres://franco:12345@localhost:5432/henry_sequelize', {
-  logging: false,
-});
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
+const db = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/henry_sequelize`,
+  {
+    logging: false,
+  }
+);
 
 modelCharacter(db);
 modelAbility(db);
@@ -14,5 +20,5 @@ modelRole(db);
 module.exports = {
   ...db.models,
   db,
-  Op
-}
+  Op,
+};
